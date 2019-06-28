ca1b61b9597cebdff8e35ed6eccfb663e280bb8d
gunerfurkanilker <gunerilkerfurkan@gmail.com>
2019-06-28 22:01:42 +0300

    Add files via upload

---
+++ arduinomega.ino
@@ -0,0 +1,271 @@
+
+#include <SoftwareSerial.h>
+
+// software serial : TX = digital pin 10, RX = digital pin 11
+//SoftwareSerial mySerial(18, 19); // RX, TX
+//SoftwareSerial mySerial2(17, 16);
+
+
+
+#include "DHT.h"
+#define DHTPIN 36     
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+int userFanPin=48;//Kullanıcı Taraflı Fan Kullanımı; D4
+int userCatiPin = 47;//Kullanıcı taraflı Çatı kullanımı D7
+int userSuMotoruPin=46;//Kullanıcı taraflı Su Motoru kullanımı D6;
+
+int catiFanPin = 38; // NodeMCU D0
+int sadeceFanPin = 51;//Sadece Fan AÃ§ma Pini D3 / 51
+int yagmurKontrolPini = 50;//YaÄŸmur durumu Ã§Ä±kÄ±ÅŸ pini D2 / 50
+int suMotoruPin = 53;//NODEMCU D1/53
+int yagmurPin = A0;
+
+int fanPwma = 13;
+int fanAin2 = 12;
+int fanAin1 = 11;
+int fanStdby = 10;
+float nem;
+float sicaklik;
+
+int catiPwmb = 7;  
+int catiBin2 = 8; 
+int catiBin1 = 9;     
+
+boolean uCati,uFan,uSuMotoru;
+
+
+
+
+int acikSwitch = 22;
+int kapaliSwitch = 23;
+
+int suMotoruPwma = 6;
+int suMotoruAin2 = 5;
+int suMotoruAin1 = 4;
+int suMotoruStby = 3;
+int toprakPin = 32;
+
+
+
+
+void setup()  
+{
+ Serial.begin(9600);
+ Serial1.begin(9600);
+ 
+ pinMode(sadeceFanPin,INPUT);
+ pinMode(yagmurKontrolPini,OUTPUT); 
+ pinMode(suMotoruPin, OUTPUT);
+ pinMode(catiFanPin,INPUT);
+ pinMode(acikSwitch,INPUT);
+
+  pinMode (fanStdby, OUTPUT);
+  pinMode (fanPwma, OUTPUT);
+  pinMode (fanAin2, OUTPUT);
+  pinMode (fanAin1, OUTPUT);
+  
+  pinMode (catiPwmb, OUTPUT);
+  pinMode (catiBin2, OUTPUT);
+  pinMode (catiBin1, OUTPUT);
+
+  pinMode (suMotoruStby, OUTPUT);
+  pinMode (suMotoruPwma, OUTPUT);
+  pinMode (suMotoruAin2, OUTPUT);
+  pinMode (suMotoruAin1, OUTPUT); 
+  pinMode (toprakPin, INPUT); 
+
+  pinMode(userFanPin,INPUT);
+  pinMode(userCatiPin,INPUT);
+  pinMode(userSuMotoruPin,INPUT);
+  
+
+}
+
+void loop()
+{
+digitalWrite(fanStdby, HIGH);
+digitalWrite(suMotoruStby, HIGH);
+
+kullaniciTarafliDegerleriOku();
+sicaklikNemOku();
+delay(100);
+sicaklikGonder();
+delay(100);
+yagmurDurumuGonder();
+delay(100);
+sadeceFanAcKapa();
+delay(100);
+catiFanAcKapa();
+delay(100);
+toprakOku();
+delay(100);
+
+
+delay(500);
+
+}
+
+void kullaniciTarafliDegerleriOku()
+{
+  if(digitalRead(userFanPin) == HIGH) uFan=true;
+  else uFan=false;
+
+  if(digitalRead(userCatiPin) == HIGH) uCati=true;
+  else uCati=false;
+
+  if(digitalRead(userSuMotoruPin) == HIGH) uSuMotoru=true;
+  else uSuMotoru=false;
+}
+
+void toprakOku()
+{
+if(digitalRead(toprakPin) == true)
+{
+    if(!uSuMotoru)
+    {
+      analogWrite(suMotoruPwma,0);
+      digitalWrite(suMotoruPin,HIGH);
+      }
+    else
+    {
+       digitalWrite(suMotoruPin,HIGH);
+      analogWrite(suMotoruPwma,40);
+     
+        digitalWrite(suMotoruAin2,LOW);
+  digitalWrite(suMotoruAin1,HIGH);
+      }
+}
+else
+{
+ digitalWrite(suMotoruPin,LOW);
+  analogWrite(suMotoruPwma,0);
+ 
+}
+}
+
+void sicaklikNemOku()
+{
+ nem = dht.readHumidity();
+ delay(500);
+sicaklik = dht.readTemperature();
+
+}
+
+
+
+
+void sicaklikGonder()
+{
+
+String stringOne = "s";
+String string = "";
+String stringThree = stringOne + sicaklik ;
+stringThree+='-';
+stringThree+=string+nem;
+
+
+Serial.println(stringThree);  
+
+}
+
+
+
+void sadeceFanAcKapa()
+{
+  if(digitalRead(sadeceFanPin) == HIGH)
+{
+    analogWrite(fanPwma,100);
+    digitalWrite(fanAin1, HIGH);
+    digitalWrite(fanAin2, LOW);
+    uCati=false;
+    catiKapa(); 
+}
+else
+ {
+  if(digitalRead(catiFanPin) == HIGH){
+    analogWrite(fanPwma,100);
+    digitalWrite(fanAin1, HIGH);
+    digitalWrite(fanAin2, LOW);
+    }
+    else
+    {
+       if(uFan)
+        analogWrite(fanPwma,100);
+       else
+        analogWrite(fanPwma,0);
+     }
+ }
+}
+
+
+
+
+void catiFanAcKapa()
+{
+  if(digitalRead(catiFanPin) == HIGH)
+{
+    analogWrite(fanPwma,100);
+    digitalWrite(fanAin1, HIGH);
+    digitalWrite(fanAin2, LOW);
+    if(uCati)catiAc();//Çatı kullanıcı taraflı açık mı?Açıksa çatıyı aç,değilse açma
+    else catiKapa();
+}
+else
+{
+    if(uCati)catiAc();//Çatı kullanıcı taraflı açık mı?Açıksa çatıyı aç,değilse açma
+    else catiKapa();
+}
+}
+
+
+
+
+
+
+void yagmurDurumuGonder()
+{
+  if(analogRead(yagmurPin) > 100)
+{
+  digitalWrite(yagmurKontrolPini,HIGH);
+}
+else if(analogRead(yagmurPin) < 100)
+{
+  digitalWrite(yagmurKontrolPini,LOW);
+
+
+}
+}
+
+void catiAc()
+{
+  while(digitalRead(acikSwitch) == HIGH)//Açık Switche değene kadar Pwmb tam kapasite çalışıyor 
+    {
+      analogWrite(catiPwmb,255);
+      digitalWrite(catiBin2,LOW);
+      digitalWrite(catiBin1,HIGH);
+    }
+    analogWrite(catiPwmb,0);
+}
+
+void catiKapa()
+{
+  while(digitalRead(kapaliSwitch) == HIGH)//Kapalı Switche değene kadar Pwmb tam kapasite 
+    {
+      analogWrite(catiPwmb,255);
+      digitalWrite(catiBin2,HIGH);
+      digitalWrite(catiBin1,LOW);
+    }
+    analogWrite(catiPwmb,0);
+    
+}
+
+
+
+
+
+
+
+
---
+++ softwareserial.ino
@@ -0,0 +1,257 @@
+            
+
+
+//FirebaseESP8266.h must be included before ESP8266WiFi.h
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "akillisera-66d6a.firebaseio.com"
+#define FIREBASE_AUTH "s0w7b9Tko4axLWbAA2lnfqhp8mKnwIEgeZvhRuK5"
+#define WIFI_SSID "Firebase"
+#define WIFI_PASSWORD "oief2019"
+
+
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+#include <SoftwareSerial.h>
+
+int yagmurPin = D2;
+int fanPin=D3;
+int catiFanPin = D0;
+int suMotoruDurumPin=D1;
+int userFanPin = D4;
+int userCatiPin = D7;
+int userSuMotoruPin = D6;
+
+
+
+float anlikSicaklik;
+float anlikNem;
+float esikSicaklik;
+float esikNem;
+
+boolean uCati,uFan,uSuMotoru;
+boolean userFanKontrol = false;
+
+
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  
+  pinMode(catiFanPin,OUTPUT);
+  pinMode(suMotoruDurumPin,INPUT);
+  pinMode(yagmurPin,INPUT);
+  pinMode(fanPin,OUTPUT);
+
+  pinMode(userCatiPin,OUTPUT);
+  pinMode(userFanPin,OUTPUT);
+  pinMode(userSuMotoruPin,OUTPUT);
+
+  
+  
+ 
+}
+
+void loop() { // run over and over
+kullaniciTarafliDegerlerAl();
+delay(100);
+kullaniciTarafliDegerlerKontrol();
+delay(100);
+yagmurDurumu();
+delay(100);
+suMotoruDurumu();
+delay(100);
+sicaklikNemAlveYolla();
+delay(100);
+esikSicaklikNemAlYolla();
+delay(100);
+sicakligaBagliFanAcKapa();
+delay(100);
+
+}
+
+
+void kullaniciTarafliDegerlerAl()
+{
+  Firebase.getBool(firebaseData,"/uCati");
+  uCati = firebaseData.boolData();
+  delay(50);
+   Firebase.getBool(firebaseData,"/uFan");
+  uFan = firebaseData.boolData();
+  delay(50);
+   Firebase.getBool(firebaseData,"/uSuMotoru");
+  uSuMotoru = firebaseData.boolData();
+}
+
+void kullaniciTarafliDegerlerKontrol()
+{
+
+  if(uCati) digitalWrite(userCatiPin,HIGH);
+  else digitalWrite(userCatiPin,LOW);
+
+  if(uFan && userFanKontrol) digitalWrite(userFanPin,HIGH);
+  else if(userFanKontrol) digitalWrite(userFanPin,LOW);
+
+  if(uSuMotoru) digitalWrite(userSuMotoruPin,HIGH);
+  else digitalWrite(userSuMotoruPin,LOW);
+  
+}
+
+
+
+
+void yagmurDurumu()
+{
+
+   if(digitalRead(yagmurPin) == HIGH) 
+  {
+      Firebase.setBool(firebaseData,"/havaDurumu",false);//HAVA YAĞIŞLI
+  }
+ 
+  else if(digitalRead(yagmurPin) == LOW)
+  {
+      Firebase.setBool(firebaseData,"/havaDurumu",true);//HAVA AÇIK
+  }
+}
+
+
+
+void suMotoruDurumu()
+{
+  if(digitalRead(suMotoruDurumPin) == HIGH) 
+  { 
+      if(uSuMotoru)
+      Firebase.setBool(firebaseData,"/suMotoru",true);//Açık
+      else
+      Firebase.setBool(firebaseData,"/suMotoru",false);//kapalı
+      delay(100);
+      Firebase.setBool(firebaseData,"/toprakDurumu",false);//Kuru
+  }
+ 
+  else if(digitalRead(suMotoruDurumPin) == LOW)
+  {
+      Firebase.setBool(firebaseData,"/suMotoru",false);//Kapalı
+      delay(100);
+      Firebase.setBool(firebaseData,"/toprakDurumu",true);//Nemli
+  }
+}
+
+
+
+void sicakligaBagliFanAcKapa()
+{
+    if(esikSicaklik < anlikSicaklik || esikNem < anlikNem)
+{
+  userFanKontrol = false;
+  
+  //
+  if(digitalRead(D2) == HIGH)//Yagmur yagiyo
+  {
+     digitalWrite(D3,HIGH);//Sadece fan açıldı
+     digitalWrite(D0,LOW);//Çatı ve fan açık durumu kapandı
+      Firebase.setBool(firebaseData,"/fan",true);
+      Firebase.setBool(firebaseData,"/cati",false);
+      Firebase.setBool(firebaseData,"/uCati",false);
+    } 
+    
+ else
+ {
+  digitalWrite(D3,LOW);//Sadece fan çalışma durmu kapandı
+  digitalWrite(D0,HIGH);//Çatı ve fan açık durumu açıldı
+      Firebase.setBool(firebaseData,"/fan",true);
+      digitalWrite(D4,HIGH);
+      if(uCati)
+      {
+         Firebase.setBool(firebaseData,"/cati",true);
+        }
+        else Firebase.setBool(firebaseData,"/cati",false);
+     
+  }
+   
+}
+else{//Sıcaklık ve nem değeri istenilen değerlere ulaştığı zaman
+  userFanKontrol = true;
+digitalWrite(D0,LOW);//Çatı ve fan açık durumu kapandı
+digitalWrite(D3,LOW);
+Serial.println(uFan);
+if(uFan) {
+  Firebase.setBool(firebaseData,"/fan",true);
+  digitalWrite(D4,HIGH);
+  }
+else{
+   Firebase.setBool(firebaseData,"/fan",false);
+   digitalWrite(D4,LOW);
+  } 
+
+if(uCati)Firebase.setBool(firebaseData,"/cati",true);
+else Firebase.setBool(firebaseData,"/cati",false);
+     
+  if(digitalRead(D2) == HIGH)//Yagmur yagiyo
+    {
+     digitalWrite(D7,LOW);//UserFanPin LOWLANDI
+     Firebase.setBool(firebaseData,"/cati",false);
+     Firebase.setBool(firebaseData,"/uCati",false);
+    } 
+  }
+}
+
+
+
+
+
+
+
+
+void sicaklikNemAlveYolla()
+{
+  if(Serial.available() > 0)
+{
+    if(Serial.peek() == 's'){
+      Serial.read();
+      
+       String s = Serial.readString();
+       String sicaklikk = s.substring(0,s.indexOf("-"));
+       String nemm = s.substring(s.indexOf("-")+1);
+       anlikSicaklik = sicaklikk.toInt();
+       anlikNem = nemm.toInt();
+      Firebase.setFloat(firebaseData,"/anlikSicaklik",anlikSicaklik);
+      delay(200);
+     Firebase.setFloat(firebaseData,"/anlikNem",anlikNem);
+    }
+}
+ }
+
+
+
+ void esikSicaklikNemAlYolla()
+{
+if(Firebase.getFloat(firebaseData,"/esikSicaklik"))
+{
+    esikSicaklik = firebaseData.floatData();
+    
+}
+delay(500);
+if(Firebase.getFloat(firebaseData,"/esikNem"))
+{
+    esikNem = firebaseData.floatData();
+    
+}
+
+}
